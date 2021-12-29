6e76eaea123bed205f15f690107f4a7fd057ecf4
ariscybertech <ariscybertech@gmail.com>
2021-12-29 15:59:03 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/aris_calendar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aris_calendar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/jalali_calendar.dart
@@ -0,0 +1,4 @@
+library persian_date;
+
+export 'src/jalalicalendarpicker.dart';
+export 'src/datepicker.dart';
